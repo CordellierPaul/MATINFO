1355e0aedc4839c2078093001b745c86284f3990
PaulCordellier <cordellierp@gmail.com>
2023-06-15 00:06:57 +0200

    filtre attribution

--- MATINFO/AttributionWindow.xaml.cs
+++ MATINFO/AttributionWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace MATINFO
@@ -25,6 +24,8 @@ namespace MATINFO
             InitializeComponent();
 
             lvAttribution.ItemsSource = donneesActuelles.LesCategories;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvAttribution.ItemsSource);
+            view.Filter += FiltreAttribution;
 
             DataContext = this;
         }
@@ -52,6 +53,37 @@ namespace MATINFO
 
                 lvAttribution.SelectedIndex = 0;
             }
+        }
+
+        private bool FiltreAttribution(object item)
+        {
+            EstAttribue attributDansLaListe = (EstAttribue)item;
+            // Cette fonction renvoie true si attributDansLaListe à l'idMateriel et à l'idPersonnel correspondent
+            // à un des éléments Materiel et Personnel. Si la fonction renvoie true, l'attribution est affichée.
+
+            bool memeIdMaterielTrouve = false;
+
+            foreach (Materiel unMateriel in lvMateriel.SelectedItems)
+            {
+                if (attributDansLaListe.IDMateriel == unMateriel.IDMateriel)
+                {
+                    memeIdMaterielTrouve = true;
+                    break;
+                }
+            }
+
+            if (!memeIdMaterielTrouve)
+                return false;   // Ici il n'y a pas d'idMateriel qui correspond
+
+            foreach (Personnel unPersonnel in lvPersonnel.SelectedItems)
+            {
+                if (attributDansLaListe.IDPersonnel == unPersonnel.IDPersonnel)
+                {
+                    return true;    // L'idPersonnel et l'idMateriel correspondent avec l'objet attributDansLaListe
+                }
+            }
+
+            return false;   // Ici il n'y a pas d'idPersonnel qui correspond
         }
     }
 }
