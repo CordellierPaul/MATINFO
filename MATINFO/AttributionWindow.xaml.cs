7d6f7dd5b5a98f10ba29b4b83c653435dd9f386e
PaulCordellier <cordellierp@gmail.com>
2023-06-15 00:32:59 +0200

    Correction d'un bug

--- MATINFO/AttributionWindow.xaml.cs
+++ MATINFO/AttributionWindow.xaml.cs
@@ -23,9 +23,15 @@ namespace MATINFO
         {
             InitializeComponent();
 
-            lvAttribution.ItemsSource = donneesActuelles.LesCategories;
-            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvAttribution.ItemsSource);
-            view.Filter += FiltreAttribution;
+            lvMateriel.ItemsSource = donneesActuelles.LesMateriels;
+            lvAttribution.ItemsSource = donneesActuelles.LesAttributions;
+            lvPersonnel.ItemsSource = donneesActuelles.LePersonnel;
+
+            if (donneesActuelles.LesAttributions != null)   // Si la liste lesAttribution est ivde, 
+            {
+                CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvAttribution.ItemsSource);
+                view.Filter = FiltreAttribution;
+            }
 
             DataContext = this;
         }
@@ -55,35 +61,42 @@ namespace MATINFO
             }
         }
 
+        // Si cette fonction renvoie true, l'attribution est affichée.
         private bool FiltreAttribution(object item)
         {
             EstAttribue attributDansLaListe = (EstAttribue)item;
-            // Cette fonction renvoie true si attributDansLaListe à l'idMateriel et à l'idPersonnel correspondent
-            // à un des éléments Materiel et Personnel. Si la fonction renvoie true, l'attribution est affichée.
+            // true sera renvoyé si attributDansLaListe à l'idMateriel et à l'idPersonnel correspondent
+            // à un des éléments Materiel et Personnel. 
 
-            bool memeIdMaterielTrouve = false;
-
-            foreach (Materiel unMateriel in lvMateriel.SelectedItems)
+            if (lvMateriel.SelectedItem != null)    // Si quelque chose est séléctionné lvMateriel
             {
-                if (attributDansLaListe.IDMateriel == unMateriel.IDMateriel)
+                bool memeIdMaterielTrouve = false;
+
+                foreach (Materiel unMateriel in lvMateriel.SelectedItems)
                 {
-                    memeIdMaterielTrouve = true;
-                    break;
+                    if (attributDansLaListe.IDMateriel == unMateriel.IDMateriel)
+                    {
+                        memeIdMaterielTrouve = true;
+                        break;
+                    }
                 }
+
+                if (!memeIdMaterielTrouve)
+                    return false;   // Ici il n'y a pas d'idMateriel qui correspond
             }
 
-            if (!memeIdMaterielTrouve)
-                return false;   // Ici il n'y a pas d'idMateriel qui correspond
-
-            foreach (Personnel unPersonnel in lvPersonnel.SelectedItems)
+            if (lvPersonnel.SelectedItem != null)    // Si quelque chose est séléctionné lvPersonnel
             {
-                if (attributDansLaListe.IDPersonnel == unPersonnel.IDPersonnel)
+                foreach (Personnel unPersonnel in lvPersonnel.SelectedItems)
                 {
-                    return true;    // L'idPersonnel et l'idMateriel correspondent avec l'objet attributDansLaListe
+                    if (attributDansLaListe.IDPersonnel == unPersonnel.IDPersonnel)
+                        return true;    // L'idPersonnel et l'idMateriel correspondent avec l'objet attributDansLaListe
                 }
+
+                return false;   // Ici il n'y a pas d'idPersonnel qui correspond
             }
 
-            return false;   // Ici il n'y a pas d'idPersonnel qui correspond
+            return true;
         }
     }
 }
