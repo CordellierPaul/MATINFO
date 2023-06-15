2b3bb4e588bf9ebf017bdd1ad25185cd4f79593e
CordellierPaul <paul.cordellier@etu.univ-smb.fr>
2023-06-15 10:51:06 +0200

    renommage d'AttributionWindow en MainWindow

--- MATINFO/MainWindow.xaml.cs
+++ MATINFO/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using MATINFO.Metier;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +10,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MATINFO.Metier;
 
 namespace MATINFO
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour AttributionWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -26,20 +23,82 @@ namespace MATINFO
         {
             InitializeComponent();
 
-            lvGeneral.Items.Refresh();
+            lvMateriel.ItemsSource = donneesActuelles.LesMateriels;
+            lvAttribution.ItemsSource = donneesActuelles.LesAttributions;
+            lvPersonnel.ItemsSource = donneesActuelles.LePersonnel;
 
-            //AccesDonnees accesBD = new AccesDonnees();
-            //accesBD.OpenConnection();
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvAttribution.ItemsSource);
+            view.Filter = FiltreAttribution;
 
-            // Test de connexion à la base de données
-            //MessageBox.Show("Résultat de la connexion : " + accesBD.OpenConnection());
+            DataContext = this;
         }
 
         private void btnHome_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = new MainWindow();
-            Close();
-            mainWindow.Show();
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+
+        private void btSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvAttribution.SelectedItem == null)
+                return;
+
+            string messageConfirmationSupp = "Êtes-vous sûr de vouloir supprimer cette attribution ?";
+            MessageBoxResult result = MessageBox.Show(messageConfirmationSupp, "Confirmation de suppression", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes && lvAttribution.SelectedItem is EstAttribue categorie)
+            {
+                categorie.Delete();
+
+                donneesActuelles.LesAttributions.Remove(categorie);
+
+                lvAttribution.SelectedIndex = 0;
+            }
+        }
+
+        // Si cette fonction renvoie true, l'attribution est affichée.
+        private bool FiltreAttribution(object item)
+        {
+            EstAttribue attributDansLaListe = (EstAttribue)item;
+            // true sera renvoyé si attributDansLaListe à l'idMateriel et à l'idPersonnel correspondent
+            // à un des éléments Materiel et Personnel. 
+
+            if (lvMateriel.SelectedItem != null)    // Si quelque chose est séléctionné lvMateriel
+            {
+                bool memeIdMaterielTrouve = false;
+
+                foreach (Materiel unMateriel in lvMateriel.SelectedItems)
+                {
+                    if (attributDansLaListe.IDMateriel == unMateriel.IDMateriel)
+                    {
+                        memeIdMaterielTrouve = true;
+                        break;
+                    }
+                }
+
+                if (!memeIdMaterielTrouve)
+                    return false;   // Ici il n'y a pas d'idMateriel qui correspond
+            }
+
+            if (lvPersonnel.SelectedItem != null)    // Si quelque chose est séléctionné lvPersonnel
+            {
+                foreach (Personnel unPersonnel in lvPersonnel.SelectedItems)
+                {
+                    if (attributDansLaListe.IDPersonnel == unPersonnel.IDPersonnel)
+                        return true;    // L'idPersonnel et l'idMateriel correspondent avec l'objet attributDansLaListe
+                }
+
+                return false;   // Ici il n'y a pas d'idPersonnel qui correspond
+            }
+
+            return true;
+        }
+
+        private void lvMaterielPersonnel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(lvAttribution.ItemsSource).Refresh();
         }
 
         private void btCategorieWindow_Click(object sender, RoutedEventArgs e)
@@ -61,13 +120,6 @@ namespace MATINFO
             PersonnelWindow personnelWindow = new PersonnelWindow();
             Close();
             personnelWindow.Show();
-        }
-
-        private void btAttribution_Click(object sender, RoutedEventArgs e)
-        {
-            AttributionWindow attributionWindow = new AttributionWindow();
-            Close();
-            attributionWindow.Show();
         }
     }
 }
